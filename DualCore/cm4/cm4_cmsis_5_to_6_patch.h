bd714e7ab31c4c2df217dc95fb443edbb0793db7
Omar Elkhouly <omar.elkhouly@arm.com>
2024-01-15 20:21:16 +0100

    updating patches from CMSIS 5 to CMSIS 6

--- DualCore/cm4/cm4_cmsis_5_to_6_patch.h
+++ DualCore/cm4/cm4_cmsis_5_to_6_patch.h
@@ -4,4 +4,13 @@
 #define CoreDebug_DEMCR_TRCENA_Msk  DCB_DEMCR_TRCENA_Msk
 #define CoreDebug                   DCB
 
+#define NVIC->IP                    NVIC->IPSR
+
+#define SCB->SHP                    SCB->SHPR
+#define SCB->PFR                    SCB->ID_PFR
+#define SCB->DFR                    SCB->ID_DFR
+#define SCB->ADR                    SCB->ID_AFR
+#define SCB->MMFR                   SCB->ID_MMFR
+#define SCB->ISAR                   SCB->ID_ISAR       
+
 #endif
