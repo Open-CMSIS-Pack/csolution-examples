2c045bdb19b798bd5b05b061376b478bf0c29bf7
Omar Elkhouly <omar.elkhouly@arm.com>
2024-01-25 13:44:53 +0100

    updated vcpkg configuration file to accommodate most recent updated tools

--- DualCore/cm4/cmsis_core5.h
+++ DualCore/cm4/cmsis_core5.h
@@ -1,7 +1,6 @@
 #ifndef CM4_CMSIS_5_to_6_PATCH
 #define CM4_CMSIS_5_to_6_PATCH
 
-#if defined (__CM_CMSIS_VERSION_MAIN) && (__CM_CMSIS_VERSION_MAIN >= 6)
 
 #define CoreDebug_DEMCR_TRCENA_Msk      DCB_DEMCR_TRCENA_Msk
 #define CoreDebug                       DCB
@@ -131,6 +130,4 @@
 #define TPI_DEVTYPE_MajorType_Pos 	    TPIU_DEVTYPE_MajorType_Pos 
 #define TPI_DEVTYPE_MajorType_Msk 	    TPIU_DEVTYPE_MajorType_Msk 	
 	
-	
-#endif
 #endif
