e5f6b0a756dddf8bdba5544b7372de8ca72d6ac3
DomenGosek <domen.gosek@arm.com>
2024-02-15 15:31:54 +0100

    Update examples to CMSIS 6

--- DualCore/cm4/RTE/Board_Support/K32L3A60VPJ1A_cm4/peripherals.c
+++ DualCore/cm4/RTE/Board_Support/K32L3A60VPJ1A_cm4/peripherals.c
@@ -13,14 +13,20 @@
 /* clang-format off */
 /* TEXT BELOW IS USED AS SETTING FOR TOOLS *************************************
 !!GlobalInfo
-product: Peripherals v5.0
+product: Peripherals v7.0
 processor: K32L3A60xxx
+package_id: K32L3A60VPJ1A
 mcu_data: ksdk2_0
 processor_version: 0.0.1
+board: FRDM-K32L3A6
 functionalGroups:
-- name: BOARD_InitPeripherals
+- name: BOARD_InitPeripherals_cm4
+  UUID: 42767dec-2704-4811-b21b-e31c81df8bb8
   called_from_default_init: true
   selectedCore: cm4
+- name: BOARD_InitPeripherals_cm4plus
+  UUID: 2ad1928e-ffcd-48c1-b16d-1ec4f2bdddd8
+  selectedCore: cm0plus
  * BE CAREFUL MODIFYING THIS COMMENT - IT IS YAML SETTINGS FOR TOOLS **********/
 
 /* TEXT BELOW IS USED AS SETTING FOR TOOLS *************************************
@@ -28,6 +34,13 @@ component:
 - type: 'system'
 - type_id: 'system_54b53072540eeeb8f8e9343e71f28176'
 - global_system_definitions: []
+ * BE CAREFUL MODIFYING THIS COMMENT - IT IS YAML SETTINGS FOR TOOLS **********/
+
+/* TEXT BELOW IS USED AS SETTING FOR TOOLS *************************************
+component:
+- type: 'msg'
+- type_id: 'msg_6e2baaf3b97dbeef01c0043275f9a0e7'
+- global_messages: []
  * BE CAREFUL MODIFYING THIS COMMENT - IT IS YAML SETTINGS FOR TOOLS **********/
 /* clang-format on */
 
@@ -39,7 +52,7 @@ component:
 /***********************************************************************************************************************
  * Initialization functions
  **********************************************************************************************************************/
-void BOARD_InitPeripherals(void)
+void BOARD_InitPeripherals_cm4(void)
 {
 }
 
@@ -48,5 +61,5 @@ void BOARD_InitPeripherals(void)
  **********************************************************************************************************************/
 void BOARD_InitBootPeripherals(void)
 {
-  BOARD_InitPeripherals();
+  BOARD_InitPeripherals_cm4();
 }
